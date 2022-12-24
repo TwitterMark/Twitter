711e7af792c3bde5cab240908fb3f90904880aa0
omalichaij <118949146+omalichaij@users.noreply.github.com>
2022-12-24 05:29:40 -0600

    Update app.java

--- app.java
+++ app.java
@@ -1,4 +1,5 @@
 # This is a code for a web application
 # Please do not hard code
-# Make use of vairables and dynamic variable
+# Make use of vairables and dynamic variables
 # author: Jenny chinedo
+# This is a front end application
